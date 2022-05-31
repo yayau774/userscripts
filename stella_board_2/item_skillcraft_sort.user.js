cf42ef6b3d83a1df47e17543c329ac7718cf0f11
yayau774 <neko@neko.jp>
2022-05-31 14:48:25 +0900

    かけら要求数順を追加

--- stella_board_2/item_skillcraft_sort.user.js
+++ stella_board_2/item_skillcraft_sort.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         stella board 2 / item skillcraft sort
 // @namespace    https://github.com/yayau774/userscripts
-// @version      0.1
+// @version      0.2
 // @description  スキルを作るセレクトボックスにソートボタンを添える
 // @author       Yayau
 // @match        http://stella2.428.st/?mode=item
@@ -11,9 +11,13 @@
 (function() {
     'use strict';
   
-    // セレクトボックスとその中身を取得
+    // セレクトボックスとその中身を取得　要求数順ソートのためにreq_sumも用意しておく
     const sel  = document.querySelector("select[name=sno]");
-    const selary = Array.from(sel.querySelectorAll("option"));
+    const selary = Array.from(sel.querySelectorAll("option")).map( e => {
+        let r = e.textContent.match(/x\d+/g) ?? [];
+        e.req_sum = r.reduce( (sum, e) => sum + parseInt(e.slice(1)), 0);
+        return e;
+    });
 
     //  通常順ボタン　ソートはvalueを見る　数値扱いするからparseIntする
     const btn_sno = document.createElement("button");
@@ -33,12 +37,22 @@
         selary.forEach(e => sel.appendChild(e));
     });
 
+    //  かけらの合計要求数順ボタン　ソートはreq_sumを見る
+    const btn_req = document.createElement("button");
+    btn_req.type = "button";
+    btn_req.textContent = "要求数順";
+    btn_req.addEventListener("click", e => {
+        selary.sort((a, b) => a.req_sum - b.req_sum);
+        selary.forEach(e => sel.appendChild(e));
+    });
+
     //  ぼたん追加
     const f = sel.closest("form");
     const p = f.insertBefore(document.createElement("p"), f.firstChild);
     p.classList.add("yy-skillcraft-sort");
     p.appendChild(btn_sno);
     p.appendChild(btn_name);
+    p.appendChild(btn_req);
 
     //  head最後にスタイルシートを追加
     document.querySelector('head').insertAdjacentHTML('beforeend', `
